8d5a7007d00604969ad895eb30f2dac2075f28b8
sugarme <thangtran@hotmail.com>
2020-08-06 13:26:19 +1000

    feat(pretokenizer/bert): added bert pretokenizer with unit test

--- pretokenizer/bert.go
+++ pretokenizer/bert.go
@@ -1,1 +1,111 @@
 package pretokenizer
+
+import (
+	"unicode"
+
+	"github.com/sugarme/tokenizer/normalizer"
+	"github.com/sugarme/tokenizer/tokenizer"
+)
+
+func isBertPunc(x rune) (retVal bool) {
+	// TODO. check whether it covers all?
+	return unicode.IsPunct(x)
+}
+
+type shouldSplitFn func(x rune) bool
+
+// splitOn splits the given string if satisfying `shouldSplit` function and
+// keeps track of the offsets.
+func splitOn(s string, shouldSplit shouldSplitFn, includeSplitToken bool) (retVal []tokenizer.PreToken) {
+
+	var (
+		words  []tokenizer.PreToken
+		offset uint   = 0
+		word   []rune = make([]rune, 0)
+	)
+
+	for _, r := range s {
+		if shouldSplit(r) {
+			if len(word) > 0 {
+				offsets := tokenizer.Offsets{
+					Start: offset - uint(len(word)),
+					End:   offset,
+				}
+				words = append(words, tokenizer.PreToken{
+					Value:   string(word),
+					Offsets: offsets,
+				})
+				word = make([]rune, 0)
+			}
+
+			if includeSplitToken {
+				offsets := tokenizer.Offsets{
+					Start: offset,
+					End:   offset + 1,
+				}
+				words = append(words, tokenizer.PreToken{
+					Value:   string([]rune{r}),
+					Offsets: offsets,
+				})
+			}
+		} else {
+			word = append(word, r)
+		}
+
+		offset += 1
+	}
+
+	// Potential last word
+	if len(word) > 0 {
+		offsets := tokenizer.Offsets{
+			Start: offset - uint(len(word)),
+			End:   offset,
+		}
+
+		words = append(words, tokenizer.PreToken{
+			Value:   string(word),
+			Offsets: offsets,
+		})
+		word = make([]rune, 0)
+	}
+
+	return words
+}
+
+type BertTokenizer struct{}
+
+// Implement PreTokenizer interface for BertTokenizer:
+// ===================================================
+
+func (bt *BertTokenizer) PreTokenize(normalized *normalizer.Normalized) (retVal []tokenizer.PreToken) {
+
+	var splitTokens []tokenizer.PreToken
+
+	shouldSplit := func(r rune) bool {
+		return unicode.IsSpace(r)
+	}
+
+	preToks := splitOn(normalized.GetNormalized(), shouldSplit, false)
+	for _, preTok := range preToks {
+		token := preTok.Value
+		offsets := preTok.Offsets
+
+		splitToksTmp := splitOn(token, isBertPunc, true)
+
+		var splitToks []tokenizer.PreToken
+		for _, splitTok := range splitToksTmp {
+			tok := splitTok.Value
+			offStart := splitTok.Offsets.Start + offsets.Start
+			offEnd := splitTok.Offsets.End + offsets.Start
+
+			splitToks = append(splitToks, tokenizer.PreToken{
+				Value:   tok,
+				Offsets: tokenizer.Offsets{Start: offStart, End: offEnd},
+			})
+		}
+
+		splitTokens = append(splitTokens, splitToks...)
+	}
+
+	return splitTokens
+}
---
+++ pretokenizer/bert_test.go
@@ -0,0 +1,36 @@
+package pretokenizer_test
+
+import (
+	"reflect"
+	"testing"
+
+	normalizer "github.com/sugarme/tokenizer/normalizer"
+	"github.com/sugarme/tokenizer/pretokenizer"
+	tokenizer "github.com/sugarme/tokenizer/tokenizer"
+)
+
+func TestBertPreTokenize(t *testing.T) {
+
+	var preTok pretokenizer.BertTokenizer
+
+	input := normalizer.NewNormalizedFrom("Hey friend!     How are you?!?")
+
+	got := preTok.PreTokenize(input)
+
+	want := []tokenizer.PreToken{
+		{Value: "Hey", Offsets: tokenizer.Offsets{Start: 0, End: 3}},
+		{Value: "friend", Offsets: tokenizer.Offsets{Start: 4, End: 10}},
+		{Value: "!", Offsets: tokenizer.Offsets{Start: 10, End: 11}},
+		{Value: "How", Offsets: tokenizer.Offsets{Start: 16, End: 19}},
+		{Value: "are", Offsets: tokenizer.Offsets{Start: 20, End: 23}},
+		{Value: "you", Offsets: tokenizer.Offsets{Start: 24, End: 27}},
+		{Value: "?", Offsets: tokenizer.Offsets{Start: 27, End: 28}},
+		{Value: "!", Offsets: tokenizer.Offsets{Start: 28, End: 29}},
+		{Value: "?", Offsets: tokenizer.Offsets{Start: 29, End: 30}},
+	}
+
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("Want:\n%v\n Got:\n%v\n", want, got)
+	}
+
+}
